af07b9aac6bbc14b34459bfe05d158b37f6fc9a7
minahan <mina.han@gmail.com>
2016-03-30 03:07:10 +0000

    added boostrap i think lol

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,9 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require bootstrap
+//= require modernizr
+//= require scripts
+//= require waypoints.min.js
+//= require contacts
+//= require info

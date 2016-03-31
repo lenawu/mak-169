5c8be08409888fd25960d56ad2ee5821492b5dfc
minahan <mina.han@gmail.com>
2016-03-31 19:17:51 +0000

    added simple company model

---
+++ app/models/company.rb
@@ -0,0 +1,5 @@
+class Company < ActiveRecord::Base
+    attribute :name,      :validate => true
+    attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+    attribute :description
+end

cd8eaa84e51a0ab741e0f34c3242e924148009b8
minahan <mina.han@gmail.com>
2016-04-01 00:05:28 +0000

    added relation to company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,20 @@
 class Company < ActiveRecord::Base
+    
+    has_many :projects, dependent: :destroy
+    
+    #has many projects
     attribute :name,      :validate => true
     attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
     attribute :description
+    
+    def company_params
+        params.require(:company).permit(:name, :email, :description)
+    end
+    
+    def create
+        @company = Company.new(company_params)
+        @company.save
+        redirect_to @company
+    end
+    
 end

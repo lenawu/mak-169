1a6b803980c9368260067d983d9a596485f192ea
straversi <straversi@berkeley.edu>
2016-04-01 01:34:59 +0000

    Remove commented material to improve coverage

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,13 +1,6 @@
 class Company < ActiveRecord::Base
     
-    # has_many :projects, dependent: :destroy
-    
     has_many :projects
-    
-    
-    #attribute :name,      :validate => true
-    #attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-    #attribute :description
     
     def company_params
         params.require(:company).permit(:name, :email, :description)
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,47 +1,10 @@
 class Project < ActiveRecord::Base
 
-#Depends on how admin position is given
-#belongs_to :user, :flag => "admin"
-
 	belongs_to :company
 	
 	has_many :assignments
 	has_many :users, :through => :assignments
 
 	attr_accessor :title, :description, :spec_urls, :proj_id
-
-	#before_action :authorize_admin :only => [:add_user]
-
-		#The is_admin? should come from the user model
-		#def authorize_admin
-		#	current_user.is_admin?
-		#end
-
-	def title_change_to (string)
-		self.title = string
-	end
-
-	def description_change_to (string)
-		self.description = string
-	end
-
-	def spec_urls_change_to (strings)
-		self.spec_url = strings
-	end
-
-
-		#find_by_sql is from ActiveRecord
-		#Might need a project assignments model for this
-		#def self.identify_by (user)
-			#Project.find_by_sql("SELECT p.* FROM projects p INNER JOIN project_assignments pa ON (p.id = pa.pid) WHERE pa.user_id = #{user.id}")
-		#end
-
-		#def add_user(user)
-			#if current_user(:admin?)
-				#user.pid = self.proj_id
-			#else
-				#error
-			#end
-		#end
 
 end

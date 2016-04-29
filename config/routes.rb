40e9e0f46836ce2eddd89614a36adef1fe4cb3a5
straversi <straversi@berkeley.edu>
2016-04-29 00:15:15 +0000

    add project view, projects index links

--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,7 @@ Rails.application.routes.draw do
   
   get 'users' => 'users#index'
   get 'projects' => 'projects#index'
+  get 'projects/:id' => 'projects#show', :as => :project
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

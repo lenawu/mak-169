2468ea01cff5a28d15108242dcc6800d91f86f05
straversi <straversi@berkeley.edu>
2016-04-29 04:49:19 +0000

    more seeds, cool projects index if company

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,18 @@
 user1 = User.create! :email => 'joe@gmail.com', :password => 'password', :admin => 'true'
 user2 = User.create! :email => 'betsy@gmail.com', :password => 'password', :admin => 'false'
 user3 = User.create! :email => 'rob@gmail.com', :password => 'password', :admin => 'false'
+user4 = User.create! :email => 'allison@gmail.com', :password => 'password', :admin => 'false'
 
 company1 = Company.create! :name => 'Google', :description => 'googling things', :email => 'google@google.com', :password => 'password'
 company2 = Company.create! :name => 'Apple', :description => 'apple stuff', :email => 'apple@apple.com', :password => 'password'
 
-project1 = Project.create! :title => 'youtube', :description => 'showing videos'
-project2 = Project.create! :title => 'android', :description => 'operating systems'
-project3 = Project.create! :title => 'iPhone', :description => 'making phones'
+project1 = Project.create! :title => 'youtube', :description => 'showing videos', :spec_urls => 'http://www.google.com/the_spec'
+project2 = Project.create! :title => 'android', :description => 'operating systems', :spec_urls => 'http://www.google.com/the_other_spec'
+project3 = Project.create! :title => 'iPhone', :description => 'making phones', :spec_urls => 'http://www.apple.com/iphone_spec'
 
 company1.projects << project1
 company1.projects << project2
 company2.projects << project3
+
+project1.users << user2
+project1.users << user3
